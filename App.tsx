558485546f3e9429810499901dff1c3cd9a7a43b
Mizumura Kosuke <kosuke@soundonlive.com>
2021-05-13 18:10:29 +0900

    added send message

--- App.tsx
+++ App.tsx
@@ -1,108 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import type { Node } from 'react'
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TouchableOpacity,
 } from 'react-native'
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen'
+  sendMessage,
+  watchEvents,
+  getReachability,
+} from 'react-native-watch-connectivity'
 
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark'
+const App: () => Node = () => {
+  const [ messages, setMessages ] = useState([])
+  const messagesRef = useRef<any>([])
+
+  const send = async () => {
+    const reachable = await getReachability()
+    console.log(reachable ? 'Watch app is reachable' : 'Watch app is not reachable')
+    if (!reachable) {
+      send()
+    }
+
+    if (reachable) {
+      sendMessage({ message: 'Hello watch!' }, (reply) => {
+        console.log(reply)
+      })
+    }
+  }
+  useEffect(() => {
+    const unsubscribe = watchEvents.on('message', (message, reply) => {
+      console.log('received message from watch', message)
+      messagesRef.current = [ ...messagesRef.current, message.message ]
+      setMessages(messagesRef.current)
+
+      if (reply) {
+        reply({ text: 'Thanks watch!' })
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}
-      >
-        {children}
-      </Text>
+    <View style={{ backgroundColor: '#fff', flex: 1 }}>
+      <SafeAreaView />
+      <StatusBar />
+      <ScrollView>
+        <Text>React Native with watch app</Text>
+        <TouchableOpacity
+          onPress={send}
+          style={{
+            backgroundColor: '#f00',
+            padding: 10,
+          }}
+        >
+          <Text>Send</Text>
+        </TouchableOpacity>
+        {
+          messages.map((mes, i) => (
+            <View key={i}>
+              <Text>{mes}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>
     </View>
   )
 }
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark'
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}
-        >
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-})
 
 export default App
--- ios/WatchApp Extension/ContentView.swift
+++ ios/WatchApp Extension/ContentView.swift
@@ -6,16 +6,68 @@
 //
 
 import SwiftUI
+import WatchKit
+import WatchConnectivity
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!!!")
-            .padding()
+final class ListViewModel: NSObject, ObservableObject {
+    @Published var messages: [String] = []
+    var session: WCSession
+    init(session: WCSession  = .default) {
+        self.session = session
+        super.init()
+        self.session.delegate = self
+        session.activate()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+extension ListViewModel: WCSessionDelegate {
+  func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    if let error = error {
+      print(error.localizedDescription)
+    } else {
+      print("The session has completed activation.")
     }
+  }
+  func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+    DispatchQueue.main.async {
+      let received = message["message"] as? String ?? "UMA"
+      self.messages.append(received)
+      print(received)
+    }
+  }
+}
+
+struct ContentView: View {
+  @ObservedObject var viewModal = ListViewModel()
+
+  var body: some View {
+    VStack() {
+      Text("Hello, World!!!")
+        .padding()
+      List {
+        Button {
+          self.sendMessage(index: "Hello iphone")
+        } label: {
+          Text("Send")
+        }
+      }
+      List {
+        ForEach(self.viewModal.messages, id: \.self) { mes in
+          Text(mes)
+        }
+      }
+      .listStyle(PlainListStyle())
+      Spacer()
+    }
+  }
+  private func sendMessage(index: String) {
+    self.viewModal.session.sendMessage([ "message": index ], replyHandler: nil) { (error) in print(error.localizedDescription)
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+      ContentView()
+  }
 }
