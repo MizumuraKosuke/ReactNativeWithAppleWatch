e49c995fd5b93bede2297082dcf51ad6a54946d4
Mizumura Kosuke <kosuke@soundonlive.com>
2021-05-18 18:23:59 +0900

    added nfc & beacon

--- App.tsx
+++ App.tsx
@@ -7,16 +7,82 @@ import {
   Text,
   View,
   TouchableOpacity,
+  DeviceEventEmitter,
 } from 'react-native'
 import {
   sendMessage,
   watchEvents,
   getReachability,
 } from 'react-native-watch-connectivity'
+import NfcManager, { NfcEvents } from 'react-native-nfc-manager'
+import Beacons from 'react-native-beacons-manager'
+
+const region = {
+  identifier: 'ibeacon-test',
+  uuid: '9c8a7cea-b59d-4f74-843a-0f84a567b131',
+}
 
 const App: () => Node = () => {
   const [ messages, setMessages ] = useState([])
   const messagesRef = useRef<any>([])
+
+  const [ distance, setDistance ] = useState(0)
+
+  const tagFound = useRef(null)
+
+  const cleanUpNfc = () => {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, null)
+    NfcManager.setEventListener(NfcEvents.SessionClosed, null)
+  }
+
+  const initNfc = async () => {
+    await NfcManager.start()
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag: any) => {
+      console.log(tag)
+      tagFound.current = tag
+      NfcManager.setAlertMessageIOS('NDEF tag found')
+      NfcManager.unregisterTagEvent().catch(() => 0)
+    })
+
+    NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
+      cleanUpNfc()
+    })
+  }
+
+  const readNfc = async () => {
+    NfcManager.registerTagEvent()
+  }
+
+  const cleanBeacon = () => {
+    Beacons.stopMonitoringForRegion(region)
+    Beacons.stopRangingBeaconsInRegion(region)
+  }
+
+  const initBeacon = async () => {
+    Beacons.requestWhenInUseAuthorization()
+  }
+
+  const startBeacon = () => {
+    Beacons.startMonitoringForRegion(region)
+    Beacons.startRangingBeaconsInRegion(region)
+    Beacons.startUpdatingLocation()
+
+
+    Beacons.BeaconsEventEmitter.addListener(
+      'didDetermineState',
+      (data: any) => {
+        console.log('didDetermineState', data)
+      },
+    )
+    Beacons.BeaconsEventEmitter.addListener(
+      'beaconsDidRange',
+      (data: any) => {
+        if (!!data && data.beacons.length > 0) {
+          setDistance(data.beacons[0].distance)
+        }
+      },
+    )
+  }
 
   const send = async () => {
     const reachable = await getReachability()
@@ -31,7 +97,10 @@ const App: () => Node = () => {
       })
     }
   }
+
   useEffect(() => {
+    initNfc()
+    initBeacon()
     const unsubscribe = watchEvents.on('message', (message, reply) => {
       console.log('received message from watch', message)
       messagesRef.current = [ ...messagesRef.current, message.message ]
@@ -44,6 +113,8 @@ const App: () => Node = () => {
 
     return () => {
       unsubscribe()
+      cleanUpNfc()
+      cleanBeacon()
     }
   }, [])
 
@@ -54,10 +125,40 @@ const App: () => Node = () => {
       <ScrollView>
         <Text>React Native with watch app</Text>
         <TouchableOpacity
+          onPress={readNfc}
+          style={{
+            backgroundColor: '#f00',
+            padding: 20,
+            margin: 20,
+          }}
+        >
+          <Text>Read NFC</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={startBeacon}
+          style={{
+            backgroundColor: '#f00',
+            padding: 20,
+            margin: 20,
+          }}
+        >
+          <Text>Start Beacon</Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            backgroundColor: '#eee',
+            padding: 10,
+            marginHorizontal: 80,
+          }}
+        >
+          <Text>distance: {distance}</Text>
+        </View>
+        <TouchableOpacity
           onPress={send}
           style={{
             backgroundColor: '#f00',
-            padding: 10,
+            padding: 20,
+            margin: 20,
           }}
         >
           <Text>Send</Text>
